7115eae29f1e86c821233129b66ae9d0cdc0c5ac
dominikJon <dominikjonik6@gmail.com>
2026-05-12 22:24:26 +0200

    create records, reports and schedule views

---
+++ frontend/src/views/ScheduleView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="view-placeholder">
+    <h1>Harmonogram (W budowie 🚧)</h1>
+  </div>
+</template>
+
+<style scoped>
+.view-placeholder { padding: 20px; }
+</style>
